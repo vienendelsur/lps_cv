49a53b6f8ca030ca60aeb3bc85777a8551f0007c
Patrick Isola <isola.patrick@gmail.com>
2017-07-11 14:58:56 +0200

    20e commit
    20e commit variable session une page

--- admin/loisirs.php
+++ admin/loisirs.php
@@ -1,4 +1,32 @@
 <?php require '../connexion/connexion.php' ?>
+<?php
+	
+session_start();// à mettre dans toutes les pages de l'admin ; SESSION et authentification
+	if(isset($_SESSION['connexion']) && $_SESSION['connexion']=='connecté'){
+		$id_utilisateur=$_SESSION['id_utilisateur'];
+		$prenom=$_SESSION['prenom'];	
+		$nom=$_SESSION['nom'];
+		
+		//echo $_SESSION['connexion'];
+		
+	}else{//l'utilisateur n'est pas connecté
+		header('location:authentification.php');
+	}
+//pour se déconnecter
+if(isset($_GET['quitter'])){// on récupère le terme quitter dans l'url
+	
+	$_SESSION['connexion']='';// on vide les variables de session
+	$_SESSION['id_utilisateur']='';
+	$_SESSION['prenom']='';
+	$_SESSION['nom']='';
+	
+	unset($_SESSION['connexion']);
+	session_destroy();
+	
+	header('location:../index.php');
+}
+
+	?>
 <?php 
 	//gestion des contenus
 	//insertion d'un loisir
